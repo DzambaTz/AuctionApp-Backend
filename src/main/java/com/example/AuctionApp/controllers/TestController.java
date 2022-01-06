09b836cf572504c00e2a658dbcab80d86ee51226
DzambaTz <tarik.dzambic@gmail.com>
2022-01-06 10:54:51 +0100

    Added status check endpoint for devops

--- src/main/java/com/example/AuctionApp/controllers/TestController.java
+++ src/main/java/com/example/AuctionApp/controllers/TestController.java
@@ -6,6 +6,8 @@
 
 package com.example.AuctionApp.controllers;
 
+import com.example.AuctionApp.payload.response.MessageResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +27,11 @@ public class TestController {
     @PreAuthorize("hasRole('SELLER')")
     public String userAccess() {
         return "Seller Content.";
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<MessageResponse> checkBackendStatus(){
+        return ResponseEntity.ok(new MessageResponse("Backend is online!"));
     }
 }
 
